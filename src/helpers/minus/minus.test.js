dcbd3a04b3d30fe8888e7164e0ae63bbcd1a5efd
Illia <ilja.golub@icloud.com>
2022-07-12 00:18:35 +0300

    test

--- src/helpers/minus/minus.test.js
+++ src/helpers/minus/minus.test.js
@@ -2,7 +2,7 @@ import {minus} from "./minus";
 
 describe('minus', function () {
     test('positive', () => {
-        expect(minus(5, 5)).toBe(0);
+        expect(minus(5, 5)).toBe(1);
     })
 
     test('negative', () => {
